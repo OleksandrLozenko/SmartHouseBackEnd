591d42413fc62154f3212542d16b82ea3b7321f5
Oleksandr Lozenko <dendroscript@gmail.com>
2024-12-04 12:26:14 +0200

    Add .gitignore file

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Command(BaseModel):
+    action: str
+
+origins = [
+#    "http://localhost:3000",
+#    "http://192.168.255.219:3000",
+    "https://oleksandrlozenko.github.io"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/toggle")
+async def toggle_light(command: Command):
+    if command.action == "on":
+        print("Лампочка включена")
+        return {"message": "Лампочка включена"}
+    elif command.action == "off":
+        print("Лампочка выключена")
+        return {"message": "Лампочка выключена"}
+    else:
+        raise HTTPException(status_code=400, detail="Неверная команда")
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
